49c0577a322d87f52d9ceec04dbfdb013e9e952c
OlisDS <cofish@bellsouth.net>
2016-06-21 10:28:26 -0400

    Removed menu and stuffs
    Renamed menu file and updated code

--- Menu Test 6-21 9-23.py
+++ Menu Test 6-21 9-23.py
@@ -4,3 +4,8 @@ from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((500,800))
 pygame.display.set_caption("CataBullet")
+
+TiR=Rect((50,700),(400,200))
+SbR=Rect((100,400),(300,150))
+Title = pygame.draw.Rect(screen, [255,0,0], TiR, width=0)
+StartButton = pygame.draw.Rect(screen, [255,0,0], SbR, width=0)
