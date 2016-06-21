2cb11567e47541da3af4a621a2ce7c862d535bd3
Jan <jan.lin.miami@gmail.com>
2016-06-21 15:13:18 -0400

    working shooter

--- shootingGame.py
+++ shootingGame.py
@@ -1,6 +1,29 @@
-#!/usr/bin/env python
+def load_image(name, colorkey=None):
 
-from helpers import *
+    fullname = os.path.join(data_dir, name)
+
+    try:
+
+        image = pygame.image.load(fullname)
+
+    except pygame.error:
+
+        print ('Cannot load image:', fullname)
+
+        raise SystemExit(str(geterror()))
+
+    image = image.convert()
+
+    if colorkey is not None:
+
+        if colorkey is -1:
+
+            colorkey = image.get_at((0,0))
+
+        image.set_colorkey(colorkey, RLEACCEL)
+
+    return image, image.get_rect()
+    
 
 class Space(pygame.sprite.Sprite):
     def __init__(self):
