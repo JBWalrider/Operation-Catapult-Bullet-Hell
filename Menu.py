303227bd06201d73e7820ff3f49e1d39a0a57f7f
OlisDS <cofish@bellsouth.net>
2016-06-22 14:19:05 -0400

    Took out ico and made menu
    Save me please

--- Menu.py
+++ Menu.py
@@ -1,12 +1,49 @@
-from tkinter import *
-from tkinter import ttk
+import pygame
 
-window = Tk()
-window.title("Catabullet")
-window.geometry("500x800")
-window.wm_iconbitmap('SpaceshipIcon.ico')
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 800
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("Bullet Hell")
 
-startb = ttk.Button(window)
-startb.pack()
+class Option:
 
-window.mainloop() 
+    hovered = False
+    
+    def __init__(self, text, pos):
+        self.text = text
+        self.pos = pos
+        self.set_rect()
+        self.draw()
+            
+    def draw(self):
+        self.set_rend()
+        screen.blit(self.rend, self.rect)
+        
+    def set_rend(self):
+        self.rend = menu_font.render(self.text, True, self.get_color())
+        
+    def get_color(self):
+        if self.hovered:
+            return (255, 255, 255)
+        else:
+            return (100, 100, 100)
+        
+    def set_rect(self):
+        self.set_rend()
+        self.rect = self.rend.get_rect()
+        self.rect.topleft = self.pos
+
+pygame.init()
+menu_font = pygame.font.Font(None, 40)
+options = [Option("NEW GAME", (180, 400))]
+while True:
+    pygame.event.pump()
+    screen.fill((0, 0, 0))
+    for option in options:
+        if option.rect.collidepoint(pygame.mouse.get_pos()):
+            option.hovered = True
+        else:
+            option.hovered = False
+        option.draw()
+    pygame.display.update()
