8d2cb250d017475f4300bed417309017451425d5
Wesley W <wesleywa7@gmail.com>
2016-06-23 13:11:56 -0400

    Adding menu.py which I forgot to save before committing

--- Menu.py
+++ Menu.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 from pygame.locals import *
 from Controller import *
@@ -64,17 +63,3 @@ TitleScreen.convert()
 c.screen.blit(TitleScreen, (0,0))
 
 menustart()
-=======
-from tkinter import *
-from tkinter import ttk
-
-window = Tk()
-window.title("Catabullet")
-window.geometry("500x800")
-window.wm_iconbitmap('images\\SpaceshipIcon.ico')
-
-startb = ttk.Button(window)
-startb.pack()
-
-window.mainloop() 
->>>>>>> Timer
