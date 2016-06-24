2049026c036eb85595ba496650b1326bd167e58d
lewj8 <reuniclussempai@gmail.com>
2016-06-24 09:25:11 -0400

    hi

--- Controller.py
+++ Controller.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, time
 from pygame.locals import *
 from Ship import *
 from Bullet import *
@@ -56,18 +56,39 @@ class Controller:
 
         pygame.time.set_timer(pygame.USEREVENT+1, 50)                   #Timer for bullet
         pygame.time.set_timer(pygame.USEREVENT+2, 100)                  #Timer for background
-        pygame.time.set_timer(pygame.USEREVENT+3, 250)                  #Timer for shooting
+        pygame.time.set_timer(pygame.USEREVENT+3, 350)                  #Timer for shooting
         pygame.time.set_timer(pygame.USEREVENT+4, 10)                   #Timer for moving
         pygame.time.set_timer(pygame.USEREVENT+5, 500)    #Timer for Enemy Shooting
-        pygame.time.set_timer(pygame.USEREVENT+6, 800)   #Timer for enemy spawn
+        pygame.time.set_timer(pygame.USEREVENT+6, 500)   #Timer for enemy spawn
 
         while True:
-
             for event in pygame.event.get():
                 if event.type == QUIT:
                     return
                 if event.type == USEREVENT+1:
+                    bulletList = self.bulletGroup.sprites()   
                     self.bulletGroup.update()
+                    enemyList = self.enemyGroup.sprites()
+                    for x in range(len(bulletList)):
+                        for i in range(len(enemyList)):
+                            if bulletList[x].team == 0 and pygame.sprite.collide_circle(bulletList[x], enemyList[i]):
+                                death = pygame.mixer.music.load("sounds/Enemy_Death.wav")
+                                pygame.mixer.music.play()
+                                enemyList[i].kill()
+                                bulletList[x].kill()
+                        if bulletList[x].team == 1 and pygame.sprite.collide_circle(self.ship, bulletList[x]):
+                            bulletList[x].kill()
+                            if not self.ship.invincible:
+                                self.ship.lives -= 1
+                               # self.ship.invincible = True
+                               # self.ship.invincTime =  time.time()
+                           # else:
+                               # if time.time() - self.ship.invincTime >= 2:
+                                  #  self.ship.invincible = False
+                            if self.ship.lives <= 0:
+                                return
+                            
+                    
                 if event.type == USEREVENT+2:
                     self.spaceGroup.update()
                 if event.type == USEREVENT+3:
@@ -83,9 +104,9 @@ class Controller:
                     sType = randint(0, 1)
                     #path = "images\\Enemy_" + (1+sType)
                     path = "images\\Enemy_" + str(sType) + ".png"
-                    initX = randint(0, 500)
+                    initX = randint(100, 400)
                     initDX = randint(-1, 1)
-                    initDY = randint(1, 2)
+                    initDY = randint(1, 1)
                     enemy = Enemy(path, initX, 0, initDX, initDY, sType, self)
                     self.enemyGroup.add(enemy)
                   
