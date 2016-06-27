c64435023242ce44ebbc223b0bd1f7394ea5cdc4
Wesley W <wesleywa7@gmail.com>
2016-06-27 13:50:18 -0400

    Adding boss with non-tested functions

--- Boss.py
+++ Boss.py
@@ -1,26 +1,58 @@
-import randint
+import randint, math
 
 class Boss:
-    def __init__(self, imageID):
+    def __init__(self, imageID, c):
         #Load images
         #type1 = pygame.image.load("images\\"),
         self.imageList = []
         self.image = self.imageList[imageID]
         self.rect = self.image.get_rect()
         self.health = 200
+        self.destY = 200
+        self.dx = 2
+        self.controller = c
 
-    def fan(self):
+    def spread(self):
+        for i in range(-3, 4):
+            eb = Enemy_Bullet(self.rect.center, 12, i*15)
+            self.controller.shoot(eb)
 
+    def alternatingSpread(self, startAngle):
+        for i in range(-3, 3):
+            eb = Enemy_Bullet(self.rect.center, 12, startAngle + i*15)
+            self.controller.shoot(eb)
+    
+    def hardShot(self):
+        x = math.degrees(math.atan(40/500))
+        for i in range(-7, 7):
+            eb = Enemy_Bullet(self.rect.center, 12, x + 2*i*x)
+            self.controller.shoot(eb)
 
-
-
-    def update(self):
-        attack = randint(0, 5)
+    def loseHealth(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            return True
+        else
+            return False
+            
+    def shoot(self):
+        attack = randint(0, 2)
         if attack == 0:
-            pass
+            self.alternatingSpread(37.5)
         elif attack == 1:
             self.spread()
         elif attack == 2:
-            self.randomSpread()
+            self.alternatingSpread(45)
+        elif attack == 3:
+            self.hardShot()
+            
+    def update(self):
+        if self.rect.y == self.destY:
+            if self.rect.right >= 450 or self.rect.left <= 50:
+                self.dx *= -1
+            else:
+                self.rect.move_ip(self.dx, 0)
+        else:
+            self.rect.move_ip(0, 1)
 
     
