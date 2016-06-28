1241cf2034ef2ed55f8d0cbd90d19a6cdfd5edf2
lewj8 <reuniclussempai@gmail.com>
2016-06-28 15:50:03 -0400

    for jan cuz idk

--- Controller.py
+++ Controller.py
@@ -51,7 +51,7 @@ class Controller(pygame.sprite.Sprite):
         self.shipGroup.draw(self.screen)
         self.bulletGroup.draw(self.screen)
         self.enemyGroup.draw(self.screen)
-        self.screen.blit(self.heartImage, (0,0))
+        
 
 
     def shoot(self, b):
